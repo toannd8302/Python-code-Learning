8b7e305036a0b0a5055e37ad1bcc43d66553125d
Toannd832 <toan03182@gmail.com>
2024-08-03 16:09:10 +0700

    String learning

---
+++ Bt1.py
@@ -0,0 +1,19 @@
+﻿#Yêu cầu:
+#1. Bạn hãy tạo file và đặt tên file bất kỳ nhưng tên phải được viết thường và mỗi từ cách nhau bởi
+#dấu gạch dưới ‘"_"‘, ví dụ như ‘simple_message.ipynb‘. Sau đó hãy lập trình in chuỗi ‘"Hello
+#world!"‘ ra màn hình.
+def main():
+    print('Hello World')  
+    #2. Gán một chuỗi ‘"Have a nice day!"‘ vào biến ‘message‘. Sau đó sử dụng câu lệnh ‘print‘ để hiển
+    #thị biến này ra màn hình.
+    message = 'Have a nice day'
+    print(message)
+    #3. Bạn hãy tạo một chuỗi ‘"Let’s have Tet holidays!"‘ và gán vào biến với tên ‘1_message‘. Sau đó
+    #sử dụng câu lệnh ‘print‘ để hiển thị biến này ra màn hình. Nếu kết quả hiển thị thông báo lỗi,
+    #hãy xác định lỗi này là gì và sửa như thế nào? Sau đó chạy lại chương trình.
+    
+    print_1_message = "Let's have Tet holidays" # Syntax ERROR => ad 'print_' problems has been solved 
+    print(print_1_message)
+if __name__ == '__main__': main()
+
+
---
+++ String.py
@@ -0,0 +1,17 @@
+﻿
+def main():
+    #String
+    print(''' Hợp lí là khi
+          nói là hợp nhau''')
+    help(''.replace) #Sinh ra số cách thử sử dụng hàm replace
+
+    #List
+    a_number_list = [1,2,3,4,5,6]
+    print(a_number_list)
+    
+    #Dictionary
+    a_dict = {'a':1, 'b':2}
+    print(a_dict['a']) # =1
+if __name__ == '__main__': main()
+
+
